d5bf52678cd4135cb539101906caf7431c7c1d51
sern <sern2001@hotmail.com>
2021-04-06 12:07:15 +0700

    Bruhscene

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,5 +1,27 @@
 package application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import scene.SceneController;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primarystage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		SceneController manager = new SceneController();
+
+		primarystage = manager.getMainstage();
+		primarystage.show();
+
+	}
 
 }
+
+
