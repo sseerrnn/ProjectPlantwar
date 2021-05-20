e342626fd14fdc78b18593553648b25a21ef7b85
sern <sern2001@hotmail.com>
2021-05-21 04:11:22 +0700

    projectile

--- src/components/other/Bullet.java
+++ src/components/other/Bullet.java
@@ -48,6 +48,41 @@ public abstract class Bullet extends Entity {
 //		getImageView().setLayoutX(getX());
 //		getBox().setLayoutX(getX());
 	}
+	public void projectileRight(Zombie zombie) {
+		move = new TranslateTransition();
+		move.setNode(imageView);
+		move.setToX(zombie.getX()-getX());
+		move.setDuration(Duration.seconds(timeCalculate(zombie)));
+		TranslateTransition move_y=new TranslateTransition();
+		move.play();
+		move_y.setNode(imageView);
+		move_y.setToY(-100);
+		move_y.setDuration(Duration.seconds(timeCalculate(zombie)/2));
+		move_y.play();
+		move_y.setOnFinished(e->{
+			move_y.setToY(0);
+			move_y.setDuration(Duration.seconds(timeCalculate(zombie)/2));
+			move_y.play();
+			
+		});
+		
+
+		movebox = new TranslateTransition();
+		movebox.setNode(box);
+		movebox.setToX(zombie.getX()-getX());
+		movebox.setDuration(Duration.seconds(timeCalculate(zombie)));
+
+		movebox.play();
+		setX(getX()+zombie.getX());
+	}
+
+	public int timeCalculate(Zombie zombie) {
+		int del_sx = zombie.getX() - getX();
+		int del_vx = getVelocity_x() - zombie.getVelocity_x();
+
+		return Math.abs(del_sx/del_vx)/2 ;
+
+	}
 
 	public TranslateTransition getMove() {
 		return move;
